4834af8437b181d553f34386b6d34af1869d5763
Jacques Begin <jacques.andre.begin@gmail.com>
2017-04-21 18:36:59 +0000

    adds connection to mongodb

---
+++ server/lib/mongo-db-connection.js
@@ -0,0 +1,27 @@
+const {MongoClient} = require("mongodb");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+var db;
+
+module.exports = {
+
+  connectToServer: function(callback) {
+    MongoClient.connect(MONGODB_URI, (err, database) => {
+      if (err) {
+        console.error(`Failed to connect: ${MONGODB_URI}`);
+        throw err;
+      }
+
+      console.log(`Connected to mongodb: ${MONGODB_URI}`);
+      db = database;
+      return callback(err);
+    });
+  },
+  getDb: function() {
+    return db;
+  },
+  closeDb: function() {
+    db.close();
+    console.log(`Closed connection to mongodb: ${MONGODB_URI}`);
+  }
+};
